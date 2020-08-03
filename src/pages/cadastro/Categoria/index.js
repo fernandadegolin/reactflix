4ff1e09842dda9bb1c8741da5e696b92eb4957e7
Fernanda Degolin <you@example.com>
2020-08-03 13:47:34 -0300

    Novas atualizacoes

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
+  function teste() {
+
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}  </h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(infosDosEvento) {
+        infosDosEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          nomeDaCategoria
+        ]);
+      }}>
+
 
         <label>
           Nome da Categoria:
           <input
             type="text"
+            value={nomeDaCategoria}
+            onChange={function teste(infosDosEvento) {
+              setNomeDaCategoria(infosDosEvento.target.value);
+            }}
           />
         </label>
 
@@ -21,6 +39,16 @@ function CadastroCategoria() {
           Cadastrar
         </button>
       </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
 
       <Link to="/">
         Ir para a Home
