4b109174525b730a24bb51be8105cc99c33a5dce
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-08-15 11:51:17 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://fernandadegolin.herokuapp.com/';
+  : 'https://fernandadegolin.herokuapp.com';
 
 
 export default {
