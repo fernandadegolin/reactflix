bbfbacd27a816bad8b3da4bab7a9f95626353a66
fernandadegolin <fernandadegolin@hotmail.com>
2021-10-14 15:33:01 -0300

    Ajuste pagina de info

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,42 +1,55 @@
 import React from 'react';
-import { BannerMe } from './styles.js';
-// import BG from '../../assets/galeria/DSC00965-69.jpg';
+import {
+  BannerMe, Infos, Section, Title,
+} from './styles.js';
+import Voltar from '../../assets/img/seta.png';
+import { Link } from 'react-router-dom';
 
 function Info() {
   return (
-    <div className="section">
-    <BannerMe />
+    <Section>
+      <BannerMe />
 
-      <h1>Fernanda Degolin</h1>
+      <Title>
+        <h1>
+          Fernanda Degolin
+
+          <span>
+            {' '}
+
+            {' '}
+            DRT - 34582SP
+          </span>
+        </h1>
+        <p>
+          Atriz e diretora
+        </p>
+
+      </Title>
+
+      <Infos>
+        <p>
+          Iniciei meus estudos em atuação em meados de 2001 ainda na minha cidade natal, Mogi Guaçu. Estudei com os professores de atuação e direção da cidade, montei nos anos seguintes uma cia de teatro experimental.
+        </p>
+
+        <p>
+          Em 2010 finalizei a faculdade de Administração e me mudei para São Paulo para me especializar na área de atuação e direção. Tive contato com renomados nomes do teatro e cinema. Estudei direção e atuação de atores no Instituto Stanislavsky, onde fiz mais de 12 curtas metragens com produções assinadas pela LA Film Institute.
+        </p>
+      </Infos>
       <p>
-        cidade natal - Mogi Guaçu, interior de São Paulo). Atriz e diretora.
+        Recentimente, recebi mentoria e estudos com direção de Alexandre Klemperer, diretor de novelas da Globo. Fiz parte do corpo docente da escola Namie Wihby School, ministrando aulas de interpretação para os alunos.
       </p>
-      <h3>DRT -  34582SP </h3>
 
-      
-        <p>
-          Fernanda iniciou os estudos em atuação em meados de 2001 ainda na cidade natal. Estudou com os professores de atuação e direção da cidade, montou nos anos seguintes a sua cia de teatro experimental.
-        </p>
-
-        <p>
-          Em 2010 finalizou sua faculdade de Administração e mudou-se para São Paulo para se especializar na área de atuação e direção. Teve contato com renomados nomes do teatro e cinema. Estudou direção e atuação de atores no Instituto Stanislavsky, onde realizou mais de 12 curtas metragens com produções assinadas pela LA Film Institute.
-        </p>
-
-        <p>Teve mentoria e estudos com direção de Alexandre Klemperer, diretor de novelas da Globo.</p>
-
-        <p>
-          Fez parte do corpo docente da escola Namie Wihby School, ministrando aulas de interpretação para os alunos.
-        </p>
-        <p>
-          Atuou em mais de 30 espetáculos teatrais ao longo da carreira, incluindo uma breve passagem pelo teatro musical.
-        </p>
-        <p>Hoje trabalha em sua produtora (The Mysterious) como diretora criativa, produzindo vídeos artísticos e criativos, vídeo clipes e curtas metragens.</p>
-
-        <p>
-          Está em cartaz desde 2020 com o espetáculo Fala Comigo, uma obra voltada para as plataformas online e se reinventando na pandemia.
-        </p>
-      
-    </div>
+      <p>
+        Somo nesses anos, mais de 30 espetáculos teatrais ao longo da carreira, incluindo uma breve passagem pelo teatro musical.
+      </p>
+      <p>
+        Hoje trabalho em minha produtora (The Mysterious) como diretora criativa, produzindo vídeos artísticos e criativos, vídeo clipes e curtas metragens.
+      </p>
+      <Link to="/">
+        <img src={Voltar} alt="voltar" />
+      </Link>
+    </Section>
 
   );
 }
--- src/components/Info/styles.js
+++ src/components/Info/styles.js
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
 // import bg from '../../assets/galeria/pic1.webp';
 
-export const Fotos = styled.h1`
-  /* color: var(--white);
-  text-align: center;
-   */
+export const Section = styled.div`
+  padding: 0 12rem;
+
+  p{
+    text-align: justify;
+  }
+
+  img{
+    filter: invert();
+    width: auto;
+    height: 4vh;
+    display: flex;
+    margin-left: auto;
+  }
+
+  @media (max-width: 1039px) {
+    padding: inherit;
+  }
 `;
 
 export const BannerMe = styled.section`
-    height: 39vh;
+    height: 43vh;
     width: 26vw;
     position: absolute;
     color: #fff;
@@ -16,10 +30,40 @@ export const BannerMe = styled.section`
     background-size: cover;
     opacity: 0.45;
 
-  @media (max-width: 800px) {
+  @media (max-width: 1039px) {
     position: inherit;
     width: auto;
     height: auto;
     min-height: 50vh;
+  }
+`;
+
+export const Title = styled.div`
+  margin-left: 24rem;
+
+  h1 {
+    margin-bottom: 0;
+
+    span{
+      font-size:14px;
+    }
+  }
+
+  p{
+    margin: 0;
+  }
+
+  @media (max-width: 1039px) {
+    margin: auto;
+  }
+
+`;
+
+export const Infos = styled.p`
+  margin-left: 24rem;
+  text-align: justify;
+
+  @media (max-width: 1039px) {
+    margin: auto;
   }
 `;
